4a002f3d98fee2a9fd545397e82349cdb9d626d6
Enrique Haro <enrique.haro@uabc.edu.mx>
2024-05-05 22:36:45 -0700

    cosulta_marca

--- pctica6numpy.py
+++ pctica6numpy.py
@@ -17,9 +17,30 @@ brands_array = np.array(brands)
 final_prices_array = np.array(final_prices)
 
 #Imprimimos para corroborar que se han cargado como se tiene planeado
-print("Arreglo de smartphones:", smartphones_array)
-print()
-print("Arreglo de marcas:", brands_array)
-print()
-print("Arreglo de precios finales:", final_prices_array)
-print()
+#print("Arreglo de smartphones:", smartphones_array)
+#print("Arreglo de marcas:", brands_array)
+#print("Arreglo de precios finales:", final_prices_array)
+
+
+def buscar_smartphones_por_marca(smartphones, brands_array):
+    
+    brand = input("Ingrese la marca que desea buscar: ")
+     
+    # Busca la marca en el arreglo de marcas
+    indices = np.where(brands_array == brand)[0]
+    
+    # Imprime los smartphones correspondientes a esa marca
+    if len(indices) > 0:
+        print()
+        print("Smartphones de la marca", brand, ":")
+        for idx in indices:
+            print(smartphones[idx])
+    else:
+        print("No se encontraron smartphones de la marca", brand)
+
+# Suponiendo que tienes los arreglos smartphones y brands_array disponibles
+while True:
+    buscar_smartphones_por_marca(smartphones, brands_array)
+    opcion = input("¿Desea hacer otra consulta? (s/n): ")
+    if opcion.lower() != 's':
+        break
