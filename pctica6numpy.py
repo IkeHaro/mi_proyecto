8fb237f79acd22993c398e47156fc0b6ca1b6f46
Enrique Haro <enrique.haro@uabc.edu.mx>
2024-05-05 22:23:23 -0700

    arrays

--- pctica6numpy.py
+++ pctica6numpy.py
@@ -2,19 +2,21 @@ import numpy as np
 import pandas as pd
 import csv
 
+#Cargamos archivo csv de nuestra ruta correspondiente
 ruta_csv = "C:/Users/danny/OneDrive/Documents/mi_proyecto/smartphones.csv"
 data = pd.read_csv(ruta_csv)
 
-#print(data.head(10))
-
+#Cargamos las columnas que necesitaremos de nuetro data set como listas de python
 smartphones = data['Smartphone'].tolist()
 brands = data['Brand'].tolist()
 final_prices = data['Final Price'].tolist()
 
+#Convertiremos nuestras listas en erreglos NumPy
 smartphones_array = np.array(smartphones)
 brands_array = np.array(brands)
 final_prices_array = np.array(final_prices)
 
+#Imprimimos para corroborar que se han cargado como se tiene planeado
 print("Arreglo de smartphones:", smartphones_array)
 print()
 print("Arreglo de marcas:", brands_array)
