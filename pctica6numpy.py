75828dd65991b88e4e64bf8a9834f626f64cab80
Enrique Haro <enrique.haro@uabc.edu.mx>
2024-05-05 22:12:28 -0700

    carga csv

--- pctica6numpy.py
+++ pctica6numpy.py
@@ -1,16 +1,23 @@
 import numpy as np
+import pandas as pd
 import csv
 
-# Paso 1: Cargar datos desde el archivo CSV
-phones_store = []
-with open('smartphones.csv', newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        phones_store.append(float(row['Final Price']))
+ruta_csv = "C:/Users/danny/OneDrive/Documents/mi_proyecto/smartphones.csv"
+data = pd.read_csv(ruta_csv)
 
-# Convertir la lista de ventas mensuales a un arreglo NumPy
-phones_store = np.array(phones_store)
+#print(data.head(10))
 
-print("Datos de ventas de smartphones:")
-print(phones_store)
+smartphones = data['Smartphone'].tolist()
+brands = data['Brand'].tolist()
+final_prices = data['Final Price'].tolist()
 
+smartphones_array = np.array(smartphones)
+brands_array = np.array(brands)
+final_prices_array = np.array(final_prices)
+
+print("Arreglo de smartphones:", smartphones_array)
+print()
+print("Arreglo de marcas:", brands_array)
+print()
+print("Arreglo de precios finales:", final_prices_array)
+print()
